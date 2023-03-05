6147f8200ce08d2350a2e81d2fa9ba09d1319b3f
hishovon <akshovon112@gmail.com>
2023-03-06 03:39:42 +0600

    first-commit

---
+++ js/math.js
@@ -0,0 +1,37 @@
+function start(number)
+
+{ let array = [];
+    for (let i = 1; i <= 100; i++) {
+
+        if (number % i == 0) {
+
+           array.push(i);
+
+        }
+
+
+    }
+
+    return array;
+}
+
+
+
+
+
+const inputFiled = document.getElementById('btn').addEventListener('click', function () {
+
+    const inputValue = document.getElementById('input').value;
+
+    const final = start(inputValue);
+    
+
+
+    const result = (document.getElementById('result'));
+
+    result.innerText = final;
+
+    
+})
+
+
