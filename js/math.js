33e6e5c1d4aa9433f684531a1f6e993a6634f34e
hishovon <akshovon112@gmail.com>
2023-03-06 13:53:00 +0600

    third-commit

--- js/math.js
+++ js/math.js
@@ -3,7 +3,7 @@ function start(number)
 { let array = [];
     for (let i = 1; i <= 100; i++) {
 
-        if (number % i == 0) {
+        if (number % i == 0 ) {
 
            array.push(i);
 
@@ -18,7 +18,6 @@ function start(number)
 
 
 
-
 const inputFiled = document.getElementById('btn').addEventListener('click', function () {
 
     const inputValue = document.getElementById('input').value;
@@ -29,9 +28,28 @@ const inputFiled = document.getElementById('btn').addEventListener('click', func
 
     const result = (document.getElementById('result'));
 
+    
     result.innerText = final;
 
     
 })
 
+document.getElementById("input").addEventListener("keypress", function(event){
+    
+    console.log(event.key);
+    if (event.key=== 'Enter'){
+        
+    const inputValue = document.getElementById('input').value;
+
+    const final = start(inputValue);
+    
+
+
+    const result = (document.getElementById('result'));
+
+    
+    result.innerText = final;
+
+    }
+})
 
