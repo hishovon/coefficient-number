ed44f1ce40f40dc6d5f0e61acbfb192f1c9ebc97
hishovon <akshovon112@gmail.com>
2023-03-08 14:53:53 +0600

    thirtheen-commit

--- js/square.js
+++ js/square.js
@@ -1,46 +1,20 @@
 function square(x) {
     return x * x;
+  }
   
-  
-  
-}
-
-
-
-
-const inputFiled = document.getElementById('btn').addEventListener('click', function () {
-
-    const square = document.getElementById('input').value;
-
-    const final = square(x);
-    
-
-
-    const result = (document.getElementById('result'));
-
-    
+  const inputField = document.getElementById('btn').addEventListener('click', function () {
+    const inputValue = document.getElementById('input').value;
+    const final = square(inputValue);
+    const result = document.getElementById('result');
     result.innerText = final;
-
-    
-})
-
-document.getElementById("input").addEventListener("keypress", function(event){
-    
-    console.log(event.key);
-    if (event.key=== 'Enter'){
-        
-    const x = document.getElementById('input').value;
-
-    const final = square(x);
-    
-
-
-    const result = (document.getElementById('result'));
-
-    
-    result.innerText = final;
-
+  });
+  
+  document.getElementById('input').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+      const x = document.getElementById('input').value;
+      const final = square(x);
+      const result = document.getElementById('result');
+      result.innerText = final;
     }
-})
-
-
+  });
+  
